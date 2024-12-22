90f6df8a95a788e43dcb3d04c61d7665b2057a35
Jake Soenneker <jake@soenneker.com>
2024-12-22 07:47:22 -0600

    Tests

---
+++ test/Soenneker.Constants.Auth.Tests/AuthConstantsTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Constants.Auth.Tests;
+
+public class AuthConstantsTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
