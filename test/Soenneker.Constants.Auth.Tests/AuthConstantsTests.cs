7b0050e80aabcefec6a5130039355e76eeda0527
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:29 -0500

    TUnit conversion

--- test/Soenneker.Constants.Auth.Tests/AuthConstantsTests.cs
+++ test/Soenneker.Constants.Auth.Tests/AuthConstantsTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Constants.Auth.Tests;
 
 public class AuthConstantsTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
